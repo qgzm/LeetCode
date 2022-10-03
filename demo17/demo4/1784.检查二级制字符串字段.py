efb81073d5e4fbe4a061847a430c4cdbcdc0fb77
qgzm <jhb_j@qq.com>
2022-10-03 18:15:02 +0800

    commit

---
+++ demo17/demo4/1784.检查二级制字符串字段.py
@@ -0,0 +1,5 @@
+# 使用者：姜海波
+# 创建时间：2022/10/3  18:14
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        return "01" not in s
--- demo6/demo4/672.灯泡开关二.py
+++ demo6/demo4/672.灯泡开关二.py
@@ -3,8 +3,10 @@
 class Solution:
     def flipLights(self, n: int, presses: int) -> int:
         seen = set()
-        for i in range(2**4):
+        for i in range(2 ** 4):
             pressArr = [(i >> j) & 1 for j in range(4)]
+            # pressArr表示四个按钮的按动情况
+            # 整数status表示四组灯泡亮灭的状态
             if sum(pressArr) % 2 == presses % 2 and sum(pressArr) <= presses:
                 status = pressArr[0] ^ pressArr[1] ^ pressArr[3]
                 if n >= 2:
@@ -16,3 +18,5 @@ class Solution:
                 seen.add(status)
         return len(seen)
 
+
+Solution().flipLights(4, 3)
