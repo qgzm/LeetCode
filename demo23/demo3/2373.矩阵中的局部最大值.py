7cba5c3b87f81ad8fd5047116ad505e9e62dfc6b
qgzm <jhb_j@qq.com>
2023-03-01 12:43:14 +0800

    commit

--- demo23/demo3/2373.矩阵中的局部最大值.py
+++ demo23/demo3/2373.矩阵中的局部最大值.py
@@ -5,12 +5,11 @@ from typing import List
 
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
-        class Solution:
-            def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
-                n = len(grid)
-                ans = [[0] * (n - 2) for _ in range(n - 2)]
-                for i in range(n - 2):
-                    for j in range(n - 2):
-                        ans[i][j] = max(grid[x][y] for x in range(i, i + 3) for y in range(j, j + 3))
-                return ans
+
+        n = len(grid)
+        ans = [[0] * (n - 2) for _ in range(n - 2)]
+        for i in range(n - 2):
+            for j in range(n - 2):
+                ans[i][j] = max(grid[x][y] for x in range(i, i + 3) for y in range(j, j + 3))
+        return ans
 
