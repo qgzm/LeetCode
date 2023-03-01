0543e1df5f32f00990a257f6bbde9a979cb7d9f6
qgzm <jhb_j@qq.com>
2023-03-01 15:51:41 +0800

    commit

--- demo4/demo4/491.递增子序列.py
+++ demo4/demo4/491.递增子序列.py
@@ -5,21 +5,12 @@ from typing import List
 
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        # ans = []
-        # for i in range(len(nums)):
-        #     tem = [nums[i]]
-        #     for j in range(i + 1, len(nums)):
-        #
-        #         if nums[j] >= tem[-1]:
-        #             tem.append(nums[j])
-        #
-        #             ans.append(tem)
-        # return ans
         if not nums:
             return []
         pres = {(nums[0],)}
         for i in nums[1:]:
             pres.update({j + (i,) for j in pres if j[-1] <= i})
+            # 非常重要
             pres.add((i,))
 
         return [list(i) for i in pres if len(i) > 1]
