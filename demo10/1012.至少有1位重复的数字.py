80bcbfd0f359fbaabe184d36c292d91c522e3fec
qgzm <jhb_j@qq.com>
2023-03-20 23:26:08 +0800

    commit

---
+++ demo10/1012.至少有1位重复的数字.py
@@ -0,0 +1,22 @@
+# 使用者：姜海波
+# 创建时间：2023/3/20  22:23
+from math import perm
+
+
+class Solution:
+    def numDupDigitsAtMostN(self, n: int) -> int:
+        limit=list(map(int,str(n)))
+        s=set()
+        n=len(limit)
+        res=sum(9*perm(9,i) for i in range(len(limit)-1))
+        for i,x in enumerate(limit):
+            for y in range(i==0,x):
+                if y not in s:
+                    res+=perm(9-i,n-i-1)
+            if x in s:
+                break
+            s.add(x)
+        return n-res
+
+
+Solution().numDupDigitsAtMostN(11)
